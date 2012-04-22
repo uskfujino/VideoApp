c22150b848f7ab2ef97d20446b8cc123bcbc424d
Yusuke Fujino <gm.yusuke.fujino@gmail.com>
2012-04-22 16:56:51 +0900

    MongoHQに対応

--- app.rb
+++ app.rb
@@ -9,7 +9,17 @@ DebugLog.info "debug.log created"
 
 # Mongoid設定
 Mongoid.configure do |config|
-  config.master = Mongo::Connection.new.db('bootstrap_haml')
+  mongohq_url = ENV['MONGOHQ_URL']
+  mongohq_db_name = ENV['MONGOHQ_DB']
+
+  if mongohq_url && mongohq_db_name
+    puts "MongoHQ Started"
+    conn = Mongo::Connection.from_uri(mongohq_url)
+    config.master = conn.db(mongohq_db_name)
+  else
+    puts "Local MongoDB Started"
+    config.master = Mongo::Connection.new.db('bootstrap_haml')
+  end
 end
 
 # Pusher設定
