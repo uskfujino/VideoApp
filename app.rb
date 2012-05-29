fc778774071d61c5ead1770a9bcaf02c851ab679
Yusuke Fujino <gm.yusuke.fujino@gmail.com>
2012-05-30 01:51:54 +0900

    カメラ機能のみに変更

--- app.rb
+++ app.rb
@@ -1,8 +1,6 @@
 require './config/initializers/mongodb'
 require './config/initializers/pusher'
 require './models/user'
-require './models/tweet'
-require './models/session'
 require './models/channel'
 require 'dropbox_sdk'
 require 'logger'
@@ -34,7 +32,7 @@ class MainApp < Sinatra::Base
     alias_method :h, :escape_html
 
     def project_name
-      'Sandbox Apps'
+      'Video Apps'
     end
 
     def topbar
@@ -72,45 +70,11 @@ class MainApp < Sinatra::Base
     end
   end
 
-  def find_tweets
-    if !Tweet.exists?
-      nil
-    elsif logged_in?
-      Tweet.where(user_id: session['uid']).all
-    else
-      Tweet.all
-    end
-  end
-
-  def find_tweet(id)
-    return nil unless Tweet.exists?
-
-    Tweet.find(id)
-  end
-
-  def my_tweet?(tweet)
-    if !logged_in? || tweet == nil
-      false
-    else
-      tweet.user_id == session['uid']
-    end
-  end
-
-  def delete_tweet(id)
-    tweet = find_tweet(id)
-
-    if my_tweet? tweet
-      tweet.delete
-    end
-  end
-
   get '/' do
-    tweet_page = haml :tweets, {}, :tweets => find_tweets
-
     if logged_in?
-      haml :main, {}, :pusher_key => Pusher.key, :tweet_page => tweet_page, :nickname => find_nickname
+      haml :main, {}, :pusher_key => Pusher.key, :nickname => find_nickname
     else
-      haml :welcome, {}, :tweet_page => tweet_page
+      haml :welcome
     end
   end
 
@@ -134,14 +98,11 @@ class MainApp < Sinatra::Base
   # Dropbox認証成功時に呼ばれる
   get '/auth/dropbox/callback' do
     auth = request.env['omniauth.auth']
-    #DebugLog.info auth.to_yaml
-    #DebugLog.info params.to_yaml
     session['user_name'] = auth['info']['name']
     session['uid'] = auth['provider'] + auth['uid'].to_s
     dropbox_session = DropboxSession.new(ENV['DROPBOX_APP_KEY'], ENV['DROPBOX_APP_SECRET'])
     dropbox_session.get_request_token
     dropbox_session.set_access_token(auth[:credentials][:token], auth[:credentials][:secret])
-    #dropbox_session.authorize(params)
     session[:dropbox_session] = dropbox_session.serialize
     redirect '/dropbox'
   end
@@ -160,19 +121,6 @@ class MainApp < Sinatra::Base
     puts ret
 
     ret
-=begin
-    puts params['socket_id'] + ':' + params['channel_name']
-
-    pusher_signature = HMAC::SHA256.hexdigest(Pusher.secret, params['socket_id'] + ':' + params['channel_name'])
-
-    puts pusher_signature
-
-    auth = Pusher.key + ':' + pusher_signature
-
-    puts auth
-
-    {auth: auth}.to_json
-=end
   end
 
   def logout
@@ -186,7 +134,6 @@ class MainApp < Sinatra::Base
   end
 
   def dropbox_authorized?
-    #logged_in? && DropboxSession.deserialize(session).authorized?
     if !logged_in? || session[:dropbox_session] == nil
       false
     else
@@ -234,35 +181,6 @@ class MainApp < Sinatra::Base
     redirect redirect_url
   end
 
-  post '/tweet' do
-    redirect '/' unless logged_in?
-
-    tweet = params[:post][:tweet]
-
-    if tweet == '' || tweet == nil
-      redirect '/'
-    end
-
-    Tweet.create(user_id: session['uid'], user_name: session['user_name'], time: Time.now, message: tweet)
-
-    redirect '/'
-  end
-
-  delete '/tweet/:id' do
-    puts 'delete /tweet/:id called'
-    delete_tweet(params[:id])
-    redirect '/'
-  end
-
-  get '/test_pusher' do
-    Pusher['my_channel'].trigger('my_event', {:message => 'hello world'})
-    haml :main, {}, :pusher_key => Pusher.key, :tweets => find_tweets, :nickname => find_nickname
-  end
-
-  get '/canvas' do
-    haml :play_canvas
-  end
-
   def create_camera(channel)
     camera = haml :camera, {}, :pusher_key => Pusher.key, :channel => channel
     video = haml :video, {}, :pusher_key => Pusher.key, :channels => find_channels, :uid => session['uid']
@@ -297,47 +215,6 @@ class MainApp < Sinatra::Base
 
   get '/channel' do
     haml :channel, {}, :pusher_key => Pusher.key, :channels => find_channels
-  end
-
-  get '/drive' do
-    haml :play_drive
-  end
-
-  DROPBOX_ACCESS_TYPE = :app_folder
-
-  get '/dropbox' do
-    redirect '/' unless dropbox_authorized?
-    dropbox_session = DropboxSession.deserialize(session[:dropbox_session])
-    client = DropboxClient.new(dropbox_session, DROPBOX_ACCESS_TYPE)
-    if params[:query]
-      search_result = client.search('/', params[:query], 1000)
-    else
-      search_result = client.search('/', 'txt', 1000)
-    end
-    myfolder = haml :myfolder, {}, :search_result => search_result
-    haml :play_dropbox, {}, :myfolder => myfolder
-  end
-
-  post '/dropbox/upload' do
-    redirect '/dropbox' unless dropbox_authorized?
-    dropbox_session = DropboxSession.deserialize(session[:dropbox_session])
-
-    # ACCESS_TYPE should be ':dropbox' or ':app_folder' as configured for your app
-    client = DropboxClient.new(dropbox_session, DROPBOX_ACCESS_TYPE)
-    puts "linked account:", client.account_info().inspect
-
-    file = open('./public/working-draft.txt')
-    target_file = params[:post][:filename]
-    if target_file == nil || target_file == ''
-      redirect '/dropbox'
-    end
-    target_path = '/' + target_file
-    response = client.put_file(target_path, file)
-    puts "uploaded:", response.inspect
-    file_metadata = client.metadata(target_path)
-    puts "metadata:", file_metadata.inspect
-
-    redirect '/dropbox'
   end
 
   # 無効なパスはすべてルートへ転送
